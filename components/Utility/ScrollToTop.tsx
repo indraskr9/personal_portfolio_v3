118b8e7d86fa94902fa5fd1daca73061f27b1e44
DNI9 <itsr9boi@gmail.com>
2021-04-13 13:42:35 +0530

    change style of scroll to top button

--- components/Utility/ScrollToTop.tsx
+++ components/Utility/ScrollToTop.tsx
@@ -4,11 +4,11 @@ import {useScrollValue} from 'utils/hooks'
 export const ScrollToTop = () => {
   const {scrolledHeader} = useScrollValue()
   return scrolledHeader ? (
-    <div
+    <button
       onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
-      className='fixed z-20 flex px-2 py-2 uppercase bg-white rounded-full cursor-pointer md:px-3 md:rounded-md text-dark right-5 md:right-10 bottom-5 md:bottom-10'>
+      className='fixed z-20 flex uppercase right-5 md:right-10 bottom-5 md:bottom-10 btn-white'>
       <BiUpArrowAlt size={25} />{' '}
       <p className='hidden font-medium tracking-wider md:block'>go up</p>
-    </div>
+    </button>
   ) : null
 }
